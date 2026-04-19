ae7c7e87f93b921d69a61105902abd39a30b0184
Th41 <phamdinhthai2005@gmail.com>
2026-04-19 07:25:14 +0000

    findAll actions api

--- backend/src/modules/authorization/presentation/controllers/actions/actions.controller.ts
+++ backend/src/modules/authorization/presentation/controllers/actions/actions.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('actions')
-export class ActionsController {}
+export class ActionsController {
+  @Get()
+  findAll() {
+    return 'Actions';
+  }
+}
