7fa4759e67d7dd5962dbaf23049ae978c3580597
Th41 <phamdinhthai2005@gmail.com>
2026-04-19 15:08:06 +0000

    audit log command repository

--- backend/src/modules/audit-log/audit-log.module.ts
+++ backend/src/modules/audit-log/audit-log.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuditLogTypeOrm } from './infrastructure/entities/typeorm-auditlog.entity';
+import { AUDIT_LOG_COMMAND_REPOSITORY } from './domain/repositories/audit-log-command.repository.interface';
+import { TypeOrmAuditLogCommandRepository } from './infrastructure/repositories/typeorm-audit-log-command.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([AuditLogTypeOrm])],
+  providers: [
+    {
+      provide: AUDIT_LOG_COMMAND_REPOSITORY,
+      useClass: TypeOrmAuditLogCommandRepository,
+    },
+  ],
+  exports: [AUDIT_LOG_COMMAND_REPOSITORY],
 })
 export class AuditLogModule {}
