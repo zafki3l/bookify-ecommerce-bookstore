33050729c920c551bfbe2cfb77979552f3805c64
Th41 <phamdinhthai2005@gmail.com>
2026-04-19 05:28:43 +0000

    add cache on find resource

--- backend/src/modules/authorization/application/queries/resources/find-resource.handler.ts
+++ backend/src/modules/authorization/application/queries/resources/find-resource.handler.ts
@@ -5,17 +5,32 @@ import {
   type IResourcesQueryRepository,
   RESOURCES_QUERY_REPOSITORY,
 } from '../../../domain/repositories/resources/resource-query.repository.interface';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { type Cache } from 'cache-manager';
 
 @QueryHandler(FindResourcesQuery)
 export class FindResourcesHandler implements IQueryHandler<FindResourcesQuery> {
+  private readonly TTL = 60 * 1000;
+
   constructor(
     @Inject(RESOURCES_QUERY_REPOSITORY)
     private readonly repository: IResourcesQueryRepository,
+    @Inject(CACHE_MANAGER)
+    private readonly cache: Cache,
   ) {}
 
   async execute(
     query: FindResourcesQuery,
   ): Promise<{ id: string; name: string }[]> {
-    return this.repository.find();
+    const cacheKey = `resources:${JSON.stringify(query)}`;
+
+    const cached =
+      await this.cache.get<{ id: string; name: string }[]>(cacheKey);
+    if (cached) return cached;
+
+    const resources = await this.repository.find();
+    await this.cache.set(cacheKey, resources, this.TTL);
+
+    return resources;
   }
 }
