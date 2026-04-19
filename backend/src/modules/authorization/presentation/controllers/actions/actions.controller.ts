e8e4f32a18ce4a8ee8def377b6c289a1d7048b1d
Th41 <phamdinhthai2005@gmail.com>
2026-04-19 07:26:24 +0000

    find resource api

--- backend/src/modules/authorization/presentation/controllers/actions/actions.controller.ts
+++ backend/src/modules/authorization/presentation/controllers/actions/actions.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('actions')
 export class ActionsController {
   @Get()
   findAll() {
     return 'Actions';
+  }
+
+  @Get()
+  find(@Param('id') id: string) {
+    return `Action ${id}`;
   }
 }
