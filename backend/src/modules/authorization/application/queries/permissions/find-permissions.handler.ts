972b210c4bf809c183957bd0dc298d403254901c
Th41 <phamdinhthai2005@gmail.com>
2026-04-20 08:07:28 +0000

    cache handling for find-permissions.handler

--- backend/src/modules/authorization/application/queries/permissions/find-permissions.handler.ts
+++ backend/src/modules/authorization/application/queries/permissions/find-permissions.handler.ts
@@ -5,17 +5,39 @@ import {
   type IPermissionQueryRepository,
   PERMISSIONS_QUERY_REPOSITORY,
 } from '../../../domain/repositories/permissions/permissions-query.repository.interface';
+import {
+  CACHE_REPOSITORY,
+  type ICacheRepository,
+} from '../../../../../shared/cache/domain/cache.repository.interface';
 
 @QueryHandler(FindPermissionsQuery)
 export class FindPermissionsHandler implements IQueryHandler<FindPermissionsQuery> {
+  private readonly TTL = 60 * 60 * 1000;
+
   constructor(
     @Inject(PERMISSIONS_QUERY_REPOSITORY)
     private readonly repository: IPermissionQueryRepository,
+
+    @Inject(CACHE_REPOSITORY)
+    private readonly cache: ICacheRepository,
   ) {}
   async execute(
     query: FindPermissionsQuery,
   ): Promise<{ id: string; resourceId: string; actionId: string }[]> {
+    const cacheKey = `permissions:${JSON.stringify(query)}`;
+
+    const cached =
+      await this.cache.get<
+        { id: string; resourceId: string; actionId: string }[]
+      >(cacheKey);
+    if (cached) {
+      return cached;
+    }
+
     const permissions = await this.repository.find();
+    if (permissions) {
+      await this.cache.set(cacheKey, permissions, this.TTL);
+    }
 
     return permissions;
   }
