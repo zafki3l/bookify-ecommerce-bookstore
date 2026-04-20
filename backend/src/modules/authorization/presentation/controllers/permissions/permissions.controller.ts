501c1ae57240c407b9584d03fac6d808e0658096
Th41 <phamdinhthai2005@gmail.com>
2026-04-20 05:34:48 +0000

    permissions api

--- backend/src/modules/authorization/presentation/controllers/permissions/permissions.controller.ts
+++ backend/src/modules/authorization/presentation/controllers/permissions/permissions.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
+import { CreatePermissionDto } from '../../dto/permissions/create-permission.dto';
 
 @Controller('permissions')
-export class PermissionsController {}
+export class PermissionsController {
+  @Get()
+  findAll() {
+    return 'Permissions';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `Permission with id: ${id}`;
+  }
+
+  @Post()
+  create(@Body() createPermission: CreatePermissionDto) {}
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(@Param('id') id: string) {}
+}
