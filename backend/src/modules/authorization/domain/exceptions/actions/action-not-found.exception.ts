531b11bcba5d2cc0acaf628ad6b4c553eacb416f
Th41 <phamdinhthai2005@gmail.com>
2026-04-20 06:00:41 +0000

    Update Action Exception

--- backend/src/modules/authorization/domain/exceptions/actions/action-not-found.exception.ts
+++ backend/src/modules/authorization/domain/exceptions/actions/action-not-found.exception.ts
@@ -1,8 +1,11 @@
 import { DomainException } from '../../../../../shared/exception/domain.exception';
-import { ErrorCode } from '../../../../../shared/exception/error.code';
+import { ActionErrorCode } from './action-error-code';
 
 export class ActionNotFoundException extends DomainException {
   constructor(id: string) {
-    super(`Action with id ${id} is not found`, ErrorCode.NOT_FOUND);
+    super(
+      `Action with id ${id} is not found`,
+      ActionErrorCode.ACTION_NOT_FOUND,
+    );
   }
 }
