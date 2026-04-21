93934f42c546d23cc8b7563075f56f145b7e707d
Th41 <phamdinhthai2005@gmail.com>
2026-04-21 13:48:17 +0000

    update backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   const PORT = process.env.PORT || 3000;
   await app.listen(PORT, () => {
     console.log('Running API on', PORT);
