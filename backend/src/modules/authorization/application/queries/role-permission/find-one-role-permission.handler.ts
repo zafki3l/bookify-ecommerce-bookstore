3a156e0ff4abdcdba88811ae0f86392602228c42
Th41 <phamdinhthai2005@gmail.com>
2026-04-22 08:17:14 +0000

    cache handling for find-one-role-permission.handler.ts

--- backend/src/modules/authorization/application/queries/role-permission/find-one-role-permission.handler.ts
+++ backend/src/modules/authorization/application/queries/role-permission/find-one-role-permission.handler.ts
@@ -5,17 +5,44 @@ import {
   type IRolePermissionQueryRepository,
   ROLE_PERMISSION_QUERY_REPOSITORY,
 } from '../../../domain/repositories/role-permission/role-permission-query.repository.interface';
+import {
+  CACHE_REPOSITORY,
+  type ICacheRepository,
+} from '../../../../../shared/cache/domain/cache.repository.interface';
 
 @QueryHandler(FindOneRolePermissionQuery)
 export class FindOneRolePermissionHandler implements IQueryHandler<FindOneRolePermissionQuery> {
+  private readonly TTL = 60 * 60 * 1000;
+
   constructor(
     @Inject(ROLE_PERMISSION_QUERY_REPOSITORY)
     private readonly repository: IRolePermissionQueryRepository,
+
+    @Inject(CACHE_REPOSITORY)
+    private readonly cache: ICacheRepository,
   ) {}
 
   async execute(
     query: FindOneRolePermissionQuery,
   ): Promise<{ roleId: string; permissionId: string } | null> {
-    return await this.repository.findOne(query.roleId, query.permissionId);
+    const cacheKey = `rolePermission:${query.roleId}&${query.permissionId}`;
+
+    const cached = await this.cache.get<{
+      roleId: string;
+      permissionId: string;
+    }>(cacheKey);
+    if (cached) {
+      return cached;
+    }
+
+    const rolePermission = await this.repository.findOne(
+      query.roleId,
+      query.permissionId,
+    );
+    if (rolePermission) {
+      await this.cache.set(cacheKey, rolePermission, this.TTL);
+    }
+
+    return rolePermission;
   }
 }
