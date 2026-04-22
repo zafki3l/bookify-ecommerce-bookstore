1602c27fdc9a9ce4df125b764cb9fe0f99dc6cda
Th41 <phamdinhthai2005@gmail.com>
2026-04-22 09:32:31 +0000

    update img

--- frontend/src/app/(shop)/(features)/(information)/privacy-policy/partials/privacy-archival-image.tsx
+++ frontend/src/app/(shop)/(features)/(information)/privacy-policy/partials/privacy-archival-image.tsx
@@ -11,7 +11,11 @@ export default function PrivacyArchivalImage({
   return (
     <section className="mb-16">
       <div className="relative rounded-lg overflow-hidden h-100 bg-gray-800 flex items-center justify-center">
-        <Image src={privacyImg} alt={alt} className="object-cover" fill />
+        <Image
+          src={privacyImg}
+          alt={alt}
+          className="w-full h-full object-cover"
+        />
       </div>
     </section>
   );
