409e2119662a7abc7e99fef966363b3c8b554684
Th41 <phamdinhthai2005@gmail.com>
2026-04-22 08:29:51 +0000

    cache handling for delete-role-permission.handler.ts

--- backend/src/modules/authorization/application/commands/role-permission/delete-role-permission.handler.ts
+++ backend/src/modules/authorization/application/commands/role-permission/delete-role-permission.handler.ts
@@ -5,12 +5,19 @@ import {
   type IRolePermissionCommandRepository,
   ROLE_PERMISSION_COMMAND_REPOSITORY,
 } from '../../../domain/repositories/role-permission/role-permission-command.repository.interface';
+import {
+  CACHE_REPOSITORY,
+  type ICacheRepository,
+} from '../../../../../shared/cache/domain/cache.repository.interface';
 
 @CommandHandler(DeleteRolePermissionCommand)
 export class DeleteRolePermissionHandler implements ICommandHandler<DeleteRolePermissionCommand> {
   constructor(
     @Inject(ROLE_PERMISSION_COMMAND_REPOSITORY)
     private readonly repository: IRolePermissionCommandRepository,
+
+    @Inject(CACHE_REPOSITORY)
+    private readonly cache: ICacheRepository,
   ) {}
 
   async execute(command: DeleteRolePermissionCommand): Promise<void> {
@@ -20,5 +27,9 @@ export class DeleteRolePermissionHandler implements ICommandHandler<DeleteRolePe
     );
 
     await this.repository.delete(rolePermission);
+    await this.cache.del(
+      `rolePermission:${command.roleId}&${command.permissionId}`,
+    );
+    await this.cache.del('rolePermissions:{}');
   }
 }
