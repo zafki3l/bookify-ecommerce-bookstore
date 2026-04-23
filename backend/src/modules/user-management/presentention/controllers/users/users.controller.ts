706831e034528a7c9db298ba3c73a31b469fc1e2
Th41 <phamdinhthai2005@gmail.com>
2026-04-23 01:47:53 +0000

    users api

--- backend/src/modules/user-management/presentention/controllers/users/users.controller.ts
+++ backend/src/modules/user-management/presentention/controllers/users/users.controller.ts
@@ -1,4 +1,36 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { UpdateUserDto } from '../../dto/users/update-user.dto';
+import { CreateUserDto } from '../../dto/users/create-user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  findAll() {
+    return 'all users';
+  }
+
+  @Get(':id')
+  findOne() {
+    return 'one users';
+  }
+
+  @Post()
+  create(@Body() createUser: CreateUserDto) {}
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateUser: UpdateUserDto) {}
+
+  @Delete()
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(@Param('id') id: string) {}
+}
