1bbf2bba300cba2e3a62b3431e2dd384bb685127
Th41 <phamdinhthai2005@gmail.com>
2026-04-23 01:41:55 +0000

    users controller

--- backend/src/modules/user-management/user-management.module.ts
+++ backend/src/modules/user-management/user-management.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { UsersController } from './presentention/controllers/users/users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserTypeOrm } from './infrastructure/entities/user.entity';
+import { SharedCacheModule } from '../../shared/cache/cache.module';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([UserTypeOrm]), SharedCacheModule],
+  controllers: [UsersController],
+})
 export class UserManagementModule {}
