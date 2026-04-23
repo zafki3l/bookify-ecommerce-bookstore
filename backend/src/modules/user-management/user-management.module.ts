612d5d778c9704fa7abc4e8733b5aa530bb7bc32
Th41 <phamdinhthai2005@gmail.com>
2026-04-23 03:27:13 +0000

    find-one-user.handler

--- backend/src/modules/user-management/user-management.module.ts
+++ backend/src/modules/user-management/user-management.module.ts
@@ -6,11 +6,13 @@ import { SharedCacheModule } from '../../shared/cache/cache.module';
 import { USERS_QUERY_REPOSITORY } from './domain/repositories/users-query.repository.interface';
 import { TypeOrmUsersQueryRepository } from './infrastructure/repositories/typeorm-users-query.repository';
 import { FindUsersHandler } from './application/queries/find-users.handler';
+import { FindOneUserHandler } from './application/queries/find-one-user.handler';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserTypeOrm]), SharedCacheModule],
   providers: [
     FindUsersHandler,
+    FindOneUserHandler,
     {
       provide: USERS_QUERY_REPOSITORY,
       useClass: TypeOrmUsersQueryRepository,
