40b57d4350b9cea8eb47d7f31a4bd268eede46fe
Th41 <phamdinhthai2005@gmail.com>
2026-04-24 06:03:09 +0000

    find-one-permission api

--- backend/src/modules/authorization/presentation/permissions/permissions.controller.ts
+++ backend/src/modules/authorization/presentation/permissions/permissions.controller.ts
@@ -1,20 +1,13 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  HttpCode,
-  HttpStatus,
-  Param,
-  Post,
-} from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { FindPermissionsUseCase } from '../../application/permission-use-cases/find-permissions/find-permissions.use-case';
 import { FindPermissionsResponse } from '../../application/permission-use-cases/find-permissions/find-permissions.response';
+import { FindOnePermissionUseCase } from '../../application/permission-use-cases/find-one-permission/find-one-permission.use-case';
 
 @Controller('permissions')
 export class PermissionsController {
   public constructor(
     private readonly findPermissionsUseCase: FindPermissionsUseCase,
+    private readonly findOnePermissionUseCase: FindOnePermissionUseCase,
   ) {}
 
   @Get()
@@ -25,5 +18,11 @@ export class PermissionsController {
   }
 
   @Get(':id')
-  public async findOne(@Param('id') id: string) {}
+  public async findOne(
+    @Param('id') id: string,
+  ): Promise<FindPermissionsResponse | null> {
+    const permission = await this.findOnePermissionUseCase.execute(id);
+
+    return permission;
+  }
 }
